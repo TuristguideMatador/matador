1cb7ab5bb096806913cc1bbe596a0213fd7fe231
Nelsoon-ds <davidgaming108@gmail.com>
2026-03-23 19:22:41 +0100

    Tilføjet test som active profile på vores TouristC'

--- src/test/java/com/example/matador/controller/TouristControllerTest.java
+++ src/test/java/com/example/matador/controller/TouristControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,10 +27,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@ActiveProfiles("test")
 @WebMvcTest(TouristController.class)
 class TouristControllerTest {
 
